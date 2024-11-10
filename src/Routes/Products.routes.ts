b95caca9c0ca5df1aa8d6fe50ee14d0d63dde890
SamuelGonza <samuel@tecnotics.com>
2024-11-10 15:39:34 -0500

    pr

--- src/Routes/Products.routes.ts
+++ src/Routes/Products.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { AdminAuth } from "../Auth/Admin.auth";
 import { upload } from "../Middleware/Uploads";
-import { createProduct, deleteProduct, getFilteredProducts, getProductById, getProducts, updateProduct } from "../Controllers/Products.controller";
+import { createProduct, deleteProduct, generateProductExcel, getFilteredProducts, getProductById, getProducts, updateProduct } from "../Controllers/Products.controller";
 
 
 const router: Router = Router()
@@ -13,5 +13,7 @@ router.get("/v1/product/:idProduct", AdminAuth, getProductById)
 router.post("/v1/product", AdminAuth, upload.single("image"), createProduct)
 router.put("/v1/product/:idProduct", AdminAuth, upload.single("image"), updateProduct)
 router.delete("/v1/product/:idProduct", AdminAuth, deleteProduct)
+
+router.get("/v1/inventory/download", generateProductExcel)
 
 export default router;
